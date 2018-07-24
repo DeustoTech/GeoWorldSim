c9d66055fd1e84b7eab52d6c4ab2bb67b63b9277
mairocaille <maiarrietalobo@gmail.com>
2018-07-24 13:55:05 +0200

    Added random movement of SheepAgent

--- usecase/tesis/SheepAgent.cpp
+++ usecase/tesis/SheepAgent.cpp
@@ -1,75 +1,54 @@
 #include "SheepAgent.h"
-
+#include "math.h"
 SheepAgent::SheepAgent(QObject *parent) : GWSAgent( parent ) {
     qDebug() << "PASO";
 }
 
 
-void SheepAgent::behave(){
+void SheepAgent::behave()
+{
+    //do {
+        qDebug() << "Soy oveja";
 
-    qDebug() << "Soy oveja";
+        // Get cell_X and cell_y
+        qDebug() << "Initial position = (" << this->getProperty("cell_x").toString() << ", " << this->getProperty("cell_y").toString() << ")" << endl;
 
-    /*
-     *  The behaviours we want from sheep:
-     *  1. Move
-     *      1.1 Get position
-     *      1.2 Change position to new position
-     *      1.3 Lose energy from moving
-     *      1.4 If energy < 0, die
-     *      1.5 Go to next
-     *  2. Eat
-     *      2.1 Increment energy from nourishment
-     *      2.2 Go to next
-     *  3. Reproduce
-     *      3.1 Only if another sheep in new position
-     *      3.2 Only after a given number of moves
-     *      3.3 Lose half of the energy
-     *      3.4 If energy < 0, die
-     *      3.5 Go to next
-     */
+        // Move randomly through random index generator:
+        srand ( time(NULL) ); //initialize the random seed
+        int direction[3] = {0, 1, -1}; // the possible displacements of going NORTH, SOUTH, EAST or WEST
+        int RandIndexX = rand() % 3; //generates a random number between 0 and 2
+        int RandIndexY = rand() % 3; //generates a random number between 0 and 2
 
-    /* void SheepAgent::Move()
-            {
-            energy = this->getProperty( GWSAgent::ENERGY_PROPERTY );
-            this->setProperty( GWSAgent::ENERGY_PROPERTY , energy -= energy / 4.0);
-            GWSBehaviour::getNext();
-            }
-    */
+        // Move coordinates either 0, 1 or -1 points
+        this->setProperty("cell_x", this->getProperty("cell_x").toInt() + direction[RandIndexX]);
+        this->setProperty("cell_y", this->getProperty("cell_y").toInt() + direction[RandIndexY]);
 
+        // Final position of the agent:
+        qDebug() << "Final position = (" << this->getProperty("cell_x").toString() << ", " << this->getProperty("cell_y").toString() << ")" << endl;
 
+        // Get initial energy
+        float initialEnergyFloat = this->getProperty("energy").toFloat();
+        //float nearest = roundf(val * 100) / 100;
+        // Moving consumes energy:
+        float moveLosses = initialEnergyFloat / 8.0;
 
+        // The final energy after moving:
+        float finalEnergy = initialEnergyFloat - moveLosses;
+        qDebug() << "Energy after moving = " << finalEnergy;
 
-    // Get cell_X and cell_y
-    qDebug() << "Initial abszise position = " << this->getProperty("cell_x").toString();
+        // Eating supplies energy:
+        float foodGains = finalEnergy / 12.0;
 
-    // Move x:
-    this->setProperty("cell_x", this->getProperty("cell_x").toInt() + 1);
-    qDebug() << "Final abszise position = " << this->getProperty("cell_x").toString();
+        // The final energy after eating:
+        finalEnergy = finalEnergy + foodGains;
+        qDebug() << "Energy after eating = " << finalEnergy;
 
-    // Get initial energy
-    float initialEnergyFloat = this->getProperty("energy").toFloat();
+        this-> setProperty("energy", finalEnergy);
+       // }
 
-    // Moving consumes energy:
-    float moveLosses = initialEnergyFloat / 8.0;
+    //while (this->property("energy").toFloat() >= 0.01);
 
-    // The final energy after moving:
-    float finalEnergy = initialEnergyFloat - moveLosses;
-    qDebug() << "Energy after moving = " << finalEnergy;
-
-    // Eating supplies energy:
-    float foodGains = finalEnergy / 12.0;
-
-    // The final energy after eating:
-    finalEnergy = finalEnergy + foodGains;
-    qDebug() << "Energy after eating = " << finalEnergy;
-
-    this-> setProperty("energy", finalEnergy);
-    qDebug() << "Remaining energy after moving and eating = " << this->property("energy").toString();
-
-
-
-
-
+}
     /* internal_clock = this->getInternalTime();
        if internal_clock >= reproduction_tick_threshold && another_sheep_in_cell;
             {
@@ -88,21 +67,30 @@ void SheepAgent::behave(){
     */
 
 
-    /* if this->getProperty( SheepAgent::ENERGY_PROPERTY ) <=0
-     *      {
-     *      SheepAgent::Die(  );
-     *      GWSBehaviour::getNext();
-     *      }
-     * void SheepAgent::Die(  )
-            {
-            energy = this->getProperty( SheepAgent::ENERGY_PROPERTY );
-            this->setProperty( SheepAgent::ENERGY_PROPERTY , energy == 0;);
-            GWSBehaviour::getNext();
-            }
-    */
 
+/*
+ *  The behaviours we want from sheep:
+ *  1. Move
+ *      1.1 Get position
+ *      1.2 Change position to new position
+ *      1.3 Lose energy from moving
+ *      1.4 If energy < 0, die
+ *      1.5 Go to next
+ *  2. Eat
+ *      2.1 Increment energy from nourishment
+ *      2.2 Go to next
+ *  3. Reproduce
+ *      3.1 Only if another sheep in new position
+ *      3.2 Only after a given number of moves
+ *      3.3 Lose half of the energy
+ *      3.4 If energy < 0, die
+ *      3.5 Go to next
+ */
 
-
-    this->incrementInternalTime( 5 );
-}
-
+/* void SheepAgent::Move()
+        {
+        energy = this->getProperty( GWSAgent::ENERGY_PROPERTY );
+        this->setProperty( GWSAgent::ENERGY_PROPERTY , energy -= energy / 4.0);
+        GWSBehaviour::getNext();
+        }
+*/
