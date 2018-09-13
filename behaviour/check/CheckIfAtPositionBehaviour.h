716ac3f3f6d90209b1dea34564ad5abcdb42ed8d
mairocaille <maiarrietalobo@gmail.com>
2018-09-13 11:50:08 +0200

    Added check directory

---
+++ behaviour/check/CheckIfAtPositionBehaviour.h
@@ -0,0 +1,25 @@
+#ifndef CHECKIFATPOSITION_H
+#define CHECKIFATPOSITION_H
+
+#include "../../object/ObjectFactory.h"
+#include "../../behaviour/Behaviour.h"
+
+class CheckIfAtPositionBehaviour : public GWSBehaviour
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE explicit CheckIfAtPositionBehaviour();
+
+    // PROPERTIES
+    static QString KEY_POSITION_X;
+    static QString KEY_POSITION_Y;
+
+    // GETTERS
+    virtual bool continueToNext();
+
+private slots:
+    virtual bool behave();
+};
+
+#endif // CHECKIFATPOSITION_H
