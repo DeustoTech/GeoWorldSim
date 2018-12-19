41d094f45fe67b67c88f2394a02a1eb2bbf9168f
mairocaille <maiarrietalobo@gmail.com>
2018-12-19 16:46:24 +0100

    Changes is SVM.*

--- util/svm/Svm.cpp
+++ util/svm/Svm.cpp
@@ -1,6 +1,11 @@
 #include "Svm.h"
 
+#include <QDebug>
+
 GWSSvm::GWSSvm() : GWSIntelligence (){
+}
+
+GWSSvm::~GWSSvm(){
 }
 
 
@@ -8,9 +13,19 @@ GWSSvm::GWSSvm() : GWSIntelligence (){
   METHODS
 **********************************************************************/
 
+/* NOTE [from https://www.csie.ntu.edu.tw/~cjlin/libsvm/faq.html#/Q03:_Data_preparation]:
+ * Currently libsvm supports only numerical data. You may have to change non-numerical data to numerical.
+ * For example, you can use several binary attributes to represent a categorical attribute.
+ */
+
+
+/* Train SVM on given files */
+
 void GWSSvm::train(QList<QList<QPair<QString, QVariant> > > input_train_dataset, QList<QList<QPair<QString, QVariant> > > output_train_dataset){
 
 
 
 
 }
+
+
--- util/svm/Svm.h
+++ util/svm/Svm.h
@@ -1,19 +1,26 @@
 #ifndef GWSSVM_H
 #define GWSSVM_H
 
-#include "../../util/ai/Intelligence.h"
+#include "../../../GeoWorldSim/util/ai/Intelligence.h"
+
+#include <libsvm/svm.h>
 
 class GWSSvm : public GWSIntelligence
 {
     Q_OBJECT
 
 public:
-    GWSSvm();
+    explicit GWSSvm();
+    ~GWSSvm();
 
     // METHODS
     virtual void train( QList< QList< QPair< QString , QVariant> > > input_train_dataset, QList< QList< QPair< QString , QVariant> > >  output_train_dataset ) = 0;
     virtual QJsonObject run(  QList<QPair <QString, QVariant> > inputs ) = 0;
 
+
+
+
+private:
 };
 
 #endif // GWSSVM_H
