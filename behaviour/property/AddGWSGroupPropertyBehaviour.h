fb5097d436e439b8c6185fb58c961bfb8bd1912b
mairocaille <maiarrietalobo@gmail.com>
2019-03-13 13:44:32 +0000

    Push

--- behaviour/property/AddGWSGroupPropertyBehaviour.h
+++ behaviour/property/AddGWSGroupPropertyBehaviour.h
@@ -9,11 +9,11 @@ class AddGWSGroupPropertyBehaviour : GWSBehaviour
     Q_OBJECT
 
 public:
-    explicit Q_INVOKABLE AddGWSGroupPropertyBehaviour();
+    Q_INVOKABLE explicit AddGWSGroupPropertyBehaviour();
 
     // PARAMETERS
     static QString PROPERTY_TO_ADD_TO_GWSGROUP;
-    static QString NEXT;
+    static QString NEXTS;
 
 protected slots:
 
